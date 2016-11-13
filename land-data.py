642a82c804b3da3af06f4096cd1f515c5d2e8516
Bruno Ferreira <srbruno@gmail.com>
2016-11-13 20:49:09 +0000

    code to list geojson files on data directory

--- land-data.py
+++ land-data.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+"""land-data.py: Loads GeoJSON data into a PostgreSQL database"""
 
+import os
 import json
 
+
+def get_land_files():
+    """ Get all list of all the geojson files """
+    for file in os.listdir('./data/'):
+        if file.endswith('.geojson'):
+            yield(os.path.realpath(file))
+
+
+f = get_land_files()
+for file2 in f:
+    print(file2)
